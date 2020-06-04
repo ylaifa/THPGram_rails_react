8c913350bc783977aeba3e1ee750d9e9032757a1
Yousri <laifayousri@gmail.com>
2020-06-04 18:23:34 +0200

    edit User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
-         jwt_revocation_strategy: JWTBlacklist
+         jwt_revocation_strategy: JwtBlacklist
 end
