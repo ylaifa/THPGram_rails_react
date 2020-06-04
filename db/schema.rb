f1306ea0a1f38c26150427d75c020e1ddf90f147
Yousri <laifayousri@gmail.com>
2020-06-04 16:21:07 +0200

    jwt_blacklist table migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_135048) do
+ActiveRecord::Schema.define(version: 2020_06_04_141231) do
+
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
