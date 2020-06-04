b1da6913c7afa62c559ad67bb980ab778fa22dfd
Yousri <laifayousri@gmail.com>
2020-06-04 23:15:09 +0200

    edit devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,12 +301,15 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
+    # jwt.dispatch_requests = [
+    #   ['POST', %r{^/login$}]
+    # ]
+    # jwt.revocation_requests = [
+    #   ['DELETE', %r{^/logout$}]
+    # ]
+    jwt.request_formats = {
+      user: [:json]
+    }
     jwt.expiration_time = 1.day.to_i
   end
 end
