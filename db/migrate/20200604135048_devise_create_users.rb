dabb60dc752f764af15e0340209e97d78e62506f
Yousri <laifayousri@gmail.com>
2020-06-04 16:06:43 +0200

    users table migrated

--- db/migrate/20200604135048_devise_create_users.rb
+++ db/migrate/20200604135048_devise_create_users.rb
@@ -7,6 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
